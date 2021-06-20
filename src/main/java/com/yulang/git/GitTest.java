6ea849d7fce5d970039fe90caf20215a1a1298b2
tom_gbl <goodMorning_gbl@yulang.com>
2021-06-20 12:11:31 +0800

    hot-fix commit2

--- src/main/java/com/yulang/git/GitTest.java
+++ src/main/java/com/yulang/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Holle Git!");
         System.out.println("Holle Git2!");
         System.out.println("Holle Git3!");
+        System.out.println("hot-fix Test!");
     }
 }
