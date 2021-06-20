817116dd9bc3d2d8069aaee0d8eecb24904c4dbe
tom_gbl <goodMorning_gbl@yulang.com>
2021-06-20 12:02:01 +0800

    First commit

---
+++ src/main/java/com/yulang/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yulang.git;
+
+/**
+ * @author yulang
+ * @create 2021-06-20 上午 11:43
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Holle Git!");
+    }
+}
